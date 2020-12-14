717cc5d08a4a11d65a2850eb4187871e9b582263
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-14 09:55:51 +0100

    10 Sentence Capitalization_1

--- AlgoCasts-master/exercises/capitalize/index.js
+++ AlgoCasts-master/exercises/capitalize/index.js
@@ -7,6 +7,15 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  return str
+    .split('')
+    .map((item, index, array) => {
+      return array[index - 1] === ' ' || index === 0
+        ? item.toUpperCase()
+        : item;
+    })
+    .join('');
+}
 
 module.exports = capitalize;
