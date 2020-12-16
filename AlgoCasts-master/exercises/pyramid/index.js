113c37ebe6ea0f142f2f7f8d08474325c075c19b
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-16 10:57:58 +0100

    pyramid another solution

--- AlgoCasts-master/exercises/pyramid/index.js
+++ AlgoCasts-master/exercises/pyramid/index.js
@@ -14,30 +14,48 @@
 //       ' ### '
 //       '#####'
 
-function buildBlock(n, block, res) {
-  let i = 0;
-  while (i < n) {
-    res.push(block);
-    res.unshift(block);
-    i++;
-  }
-}
-
 function pyramid(n) {
-  let i = 0;
-  let j = n - 1;
+  const middle = Math.floor((n * 2 - 1) / 2);
 
-  while (i < n) {
-    const res = ['#'];
+  for (let i = 0; i < n; i++) {
+    let level = '';
 
-    buildBlock(Math.abs(j - n) - 1, '#', res);
-    buildBlock(j, ' ', res);
+    for (let j = 0; j < n * 2 - 1; j++) {
+      if (j >= middle - i && j <= middle + i) {
+        level += '#';
+      } else {
+        level += ' ';
+      }
+    }
 
-    console.log(res.join(''));
-
-    i++;
-    j--;
+    console.log(level);
   }
 }
+
+// function buildBlock(n, block, res) {
+//   let i = 0;
+//   while (i < n) {
+//     res.push(block);
+//     res.unshift(block);
+//     i++;
+//   }
+// }
+
+// function pyramid(n) {
+//   let i = 0;
+//   let j = n - 1;
+
+//   while (i < n) {
+//     const res = ['#'];
+
+//     buildBlock(Math.abs(j - n) - 1, '#', res);
+//     buildBlock(j, ' ', res);
+
+//     console.log(res.join(''));
+
+//     i++;
+//     j--;
+//   }
+// }
 
 module.exports = pyramid;
