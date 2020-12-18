ee85bbcaaa371f765a63438bd2add2ee11df3a27
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-18 12:38:45 +0100

    matrix solution

--- AlgoCasts-master/exercises/matrix/index.js
+++ AlgoCasts-master/exercises/matrix/index.js
@@ -15,6 +15,57 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let it = n;
+  let item = 1;
+  const arr = [];
+  arr[0] = [];
+  let rowMin = 0;
+  let rowMax = n - 1;
+  let colMin = 0;
+  let colMax = n - 1;
+
+  //init
+  for (let i = 0; i < n; i++) {
+    arr[i] = [];
+    for (let j = 0; j < n; j++) {
+      arr[i].push(0);
+    }
+  }
+
+  while (item <= n * n) {
+    //right
+    for (let i = colMin; i <= colMax; i++) {
+      arr[rowMin][i] = item;
+      item++;
+    }
+    rowMin++;
+
+    //down
+    for (let i = rowMin; i <= rowMax; i++) {
+      arr[i][colMax] = item;
+      item++;
+    }
+    colMax--;
+
+    //left
+    for (let i = colMax; i >= colMin; i--) {
+      arr[rowMax][i] = item;
+      item++;
+    }
+    rowMax--;
+
+    //up
+    for (let i = rowMax; i >= rowMin; i--) {
+      arr[i][colMin] = item;
+      item++;
+    }
+    colMin++;
+  }
+
+  console.log(arr);
+  console.log(colMin, colMax, rowMin, rowMax);
+  return arr;
+}
 
 module.exports = matrix;
