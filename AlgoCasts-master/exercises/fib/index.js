8ce9d6e0abb2c9ddb3692f81ab0ff9be553d1016
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-20 07:48:42 +0100

    fibo

--- AlgoCasts-master/exercises/fib/index.js
+++ AlgoCasts-master/exercises/fib/index.js
@@ -8,6 +8,28 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+//recursive
+// function fib(n, prev = 0, act = 1) {
+//   if (n === 0) return 0;
+//   if (n === 1) return act;
+//   return fib(n - 1, act, act + prev);
+// }
+
+// //iterativ
+function fib(n) {
+  if (!n) {
+    return 0;
+  }
+  let fib = 1;
+  let act = 1;
+  let prev = 0;
+  for (let index = 1; index < n; index++) {
+    fib = act + prev;
+    prev = act;
+    act = fib;
+    // console.log(fib);
+  }
+  return fib;
+}
 
 module.exports = fib;
