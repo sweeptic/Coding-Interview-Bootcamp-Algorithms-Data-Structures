8675e19b60f28e9abe71edb1000ccb0095765c3e
Attila <59293104+sweeptic@users.noreply.github.com>
2020-12-24 11:10:17 +0100

    set up for linked list exercises

--- AlgoCasts-master/exercises/linkedlist/index.js
+++ AlgoCasts-master/exercises/linkedlist/index.js
@@ -2,7 +2,15 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+// HEAD - 0 - 0 - 0 - TAIL
+// obj: data: string. next: null
 
 class LinkedList {}
 
--- AlgoCasts-master/exercises/qfroms/index.js
+++ AlgoCasts-master/exercises/qfroms/index.js
@@ -1,5 +1,5 @@
 // --- Directions
-// Implement a Queue datastructure using two stacks.
+// Implement a Queue data structure using two stacks.
 // *Do not* create an array inside of the 'Queue' class.
 // Queue should implement the methods 'add', 'remove', and 'peek'.
 // For a reminder on what each method does, look back
@@ -14,6 +14,43 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+
+  add(element) {
+    this.stack1.push(element);
+  }
+
+  convert() {
+    while (this.stack1.peek()) {
+      this.stack2.push(this.stack1.pop());
+    }
+  }
+
+  convertBack() {
+    while (this.stack2.peek()) {
+      this.stack1.push(this.stack2.pop());
+    }
+  }
+
+  peek() {
+    this.convert();
+    const record = this.stack2.peek();
+    this.convertBack();
+    return record;
+  }
+
+  remove() {
+    this.convert();
+
+    const res = this.stack2.pop();
+
+    this.convertBack();
+    return res;
+  }
+}
 
 module.exports = Queue;
