8a53aa8929dd84fae091521c28bb5b31246410fe
unknown <sweeptic@gmail.com>
2021-01-11 13:50:41 +0100

    tree solution pt. 1

--- AlgoCasts-master/exercises/tree/index.js
+++ AlgoCasts-master/exercises/tree/index.js
@@ -10,8 +10,49 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
 
-class Tree {}
+  add(data) {
+    const node = new Node(data);
+    this.children.push(node);
+  }
+
+  remove(data) {
+    this.children = this.children.filter(node => data !== node.data);
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+  traverseBF(cb) {
+    const items = [];
+    items.push(this.root);
+
+    while (items.length) {
+      console.log(items[0].data);
+
+      cb(items[0]);
+
+      items[0].children.map(item => {
+        items.push(item);
+      });
+
+      items.shift();
+    }
+
+    //  while (items.length) {
+    //    console.log(items[0].data);
+    //    const actual = items.shift();
+    //    cb(actual);
+    //    items.push(...actual.children);
+    // }
+  }
+}
 
 module.exports = { Tree, Node };
