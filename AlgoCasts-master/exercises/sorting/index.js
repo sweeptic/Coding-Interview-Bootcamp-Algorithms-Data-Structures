c8dd5e2ea1975791ae817873fa979efe304500b8
unknown <sweeptic@gmail.com>
2021-01-22 10:38:11 +0100

    30 Sorting With BubbleSort_1

--- AlgoCasts-master/exercises/sorting/index.js
+++ AlgoCasts-master/exercises/sorting/index.js
@@ -2,19 +2,18 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+    }
+  }
+  return arr;
 }
 
-function selectionSort(arr) {
+function selectionSort(arr) {}
 
-}
+function mergeSort(arr) {}
 
-function mergeSort(arr) {
-
-}
-
-function merge(left, right) {
-
-}
+function merge(left, right) {}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
